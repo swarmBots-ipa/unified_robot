2aee11356e571404fe0dfa43ca3003e295fb2dda
ipa-rar <ragesh.ramachandran@ipa.fraunhofer.de>
2023-02-08 15:07:15 +0100

    renamed and added folders to setup.py

--- unified_robot_description/setup.py
+++ unified_robot_description/setup.py
@@ -1,6 +1,8 @@
 from setuptools import setup
+import os
+from glob import glob
 
-package_name = 'unified_description'
+package_name = 'unified_robot_description'
 
 setup(
     name=package_name,
@@ -10,6 +12,11 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+        (os.path.join('share', package_name, 'config'), glob('config/*')),
+        (os.path.join('share', package_name, 'meshes'), glob('meshes/*')),
+        (os.path.join('share', package_name, 'rviz'), glob('rviz/*')),
+        (os.path.join('share', package_name, 'urdf'), glob('urdf/*')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
